436ba9fdd854d4eb71536d60848afa4ff9a3a88c
German Rangel <gnrg_74@hotmail.com>
2023-05-15 13:28:35 -0600

    add more routes

--- backend/components/users/routes.js
+++ backend/components/users/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const controller = require('./controller');
 const router = express.Router();
+const {uploadImage} = require('../../middleware/multer');
 
+//routes
 router.get('/test',controller.testUser);
+router.post('/create',uploadImage(), controller.createUser);
+router.post('/login', controller.userLogin);
 
 module.exports = router;
