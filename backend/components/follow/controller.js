fe0cfec44605ff40ff52e2b20fb5fafbaa6d4db9
German Rangel <gnrg_74@hotmail.com>
2023-05-26 16:36:55 -0600

    follow and unfollow

--- backend/components/follow/controller.js
+++ backend/components/follow/controller.js
@@ -8,14 +8,54 @@ const test = (req,res)=>{
 
 const savefollow = (req,res)=>{
     //get dates of body
-    res.status(200).send({
-        status: 'success',
-        message: 'user followed',
-        identity: req.user
+    const params = req.body;
+    const identity = req.user;
+
+    let userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
     });
+    
+    userToFollow.save().then(data=>{
+        res.status(200).send({
+            status: 'success',
+            identity: req.user,
+            followed: data
+        });
+
+    }).catch(err =>{
+        if(err || !data){
+            return response.error(req, res, 'No follow user', 500, err);
+        }
+    });
+
+
+   
 }
 
+const unFollow = (req,res)=>{
+    const userId = req.user.id;
+
+    const followedId = req.params.id;
+    
+    Follow.find({
+        'user': userId,
+        'followed': followedId
+    }).deleteMany().then(data =>{
+
+        res.status(200).send({
+            status: 'success',
+            message: 'Follow eliminated succesfuly',
+        });
+    }).catch(err=>{
+        if(err || !data){
+        return response.error(req, res, 'You are not following this user', 401, err);
+        }
+    });
+
+}
 module.exports = {
     test,
-    savefollow
+    savefollow,
+    unFollow
 }
--- backend/components/follow/routes.js
+++ backend/components/follow/routes.js
@@ -5,4 +5,5 @@ const check = require('../../middleware/auth');
 
 router.get('/test', controller.test);
 router.post('/savefollow',check.auth, controller.savefollow);
+router.delete('/unfollow/:id',check.auth, controller.unFollow);
 module.exports = router;
