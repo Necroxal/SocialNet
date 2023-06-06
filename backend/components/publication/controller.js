2aba0b23e51142131a6820ecb7e65084a33335bf
German Rangel <gnrg_74@hotmail.com>
2023-06-06 13:10:22 -0600

    creat a view publication

--- backend/components/publication/controller.js
+++ backend/components/publication/controller.js
@@ -4,15 +4,15 @@ const test = (req, res) => {
     return response.succes(req, res, 'success', 200);
 }
 
-const savePubli = (req,res) =>{
+const savePubli = (req, res) => {
     const params = req.body;
 
-    if(!params.text) return res.status(401).send({status: 'Errpr', message: 'text doesnt exist'});
+    if (!params.text) return res.status(401).send({ status: 'Errpr', message: 'text doesnt exist' });
 
     let newPublication = new Publication(params);
     newPublication.user = req.user.id;
 
-    newPublication.save().then(data=>{
+    newPublication.save().then(data => {
 
         return res.status(200).send({
             status: 'success',
@@ -20,12 +20,28 @@ const savePubli = (req,res) =>{
             data
         });
 
-    }).catch(error =>{
-        return response.error(req,res,'Internal error',500,error);
+    }).catch(error => {
+        return response.error(req, res, 'Internal error', 500, error);
     })
+
+}
+const onePubli = (req, res) => {
+    const publicationId = req.params.id;
+
+    Publication.findById(publicationId).then(data => {
+        return res.status(200).send({
+            status: 'success',
+            message: 'Save Publication',
+            publication: data
+        });
+
+    }).catch(error => {
+        return response.error(req, res, 'There are no publications', 500, error);
+    });
 
 }
 module.exports = {
     test,
-    savePubli
+    savePubli,
+    onePubli
 }
--- backend/components/publication/routes.js
+++ backend/components/publication/routes.js
@@ -5,4 +5,5 @@ const check = require('../../middleware/auth');
 
 router.get('/test',check.auth, controller.test);
 router.post('/create', check.auth, controller.savePubli);
+router.get('/viewpublication/:id', check.auth, controller.onePubli);
 module.exports = router;
