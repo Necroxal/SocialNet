89209fa38966f64f8c827ff1d04c8c0f2420e19b
German Rangel <gnrg_74@hotmail.com>
2023-06-16 17:13:16 -0600

    create form at register

--- frontend/src/components/user/Register.jsx
+++ frontend/src/components/user/Register.jsx
@@ -8,7 +8,34 @@ export const Register = () => {
         </header>
 
         <div className="content__posts">
-           
+          <form className='register-form'>
+
+            <div className='form-group'>
+              <label htmlFor='name'>Name</label>
+              <input type='text' name='name'/>
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='surname'>Surname</label>
+              <input type='text' name='surname'/>
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='nickname'>Nickname</label>
+              <input type='text' name='nickname'/>
+            </div>
+            
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input type='text' name='email'/>
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input type='text' name='password'/>
+            </div>
+            <input type='submit' value='Sing up' className='btn btb-success' />
+          </form>
         </div>
     </>
   )
