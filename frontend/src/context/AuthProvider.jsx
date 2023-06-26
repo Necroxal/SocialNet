5db8d0dd99600fbd09f89854254a8e7dc273c3ce
German Rangel <gnrg_74@hotmail.com>
2023-06-26 12:05:22 -0600

    create context for get auth

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,13 +1,46 @@
 import React, { createContext, useState, useEffect } from 'react';
+import { Global } from '../helpers/Global';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
 
-    const [share, setShare] = useState('Share all components');
+    const [auth, setAuth] = useState({});
+
+    useEffect(()=>{
+        authUser();
+    },[]);
+
+    const authUser = async()=>{
+        //get date identify local storage
+        const token = localStorage.getItem('token');
+        const user = localStorage.getItem('user');
+
+        //check if i have the data (token and user)
+        if(!token || !user){
+            return false;
+        }
+        //Transform the data to object js
+        const userObj = JSON.parse(user);
+        const userId = userObj.id;
+        //ajax request to check the token
+        //get data profile user
+        const request = await fetch(Global.url + 'user/profile/' + userId,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        });
+
+        const data = await request.json();
+        //Status auth
+        setAuth(data.user);
+    }
     return (
         <AuthContext.Provider
             value={{
-                share
+                auth,
+                setAuth
             }}>
             {children}
         </AuthContext.Provider>
