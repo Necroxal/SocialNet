562efa619725c29500b7c55c9df62b9b0b02fb33
German Rangel <gnrg_74@hotmail.com>
2023-07-05 13:12:22 -0600

    abstraction UserLits

--- frontend/src/components/user/People.jsx
+++ frontend/src/components/user/People.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import avatar from '../../assets/img/user.png';
 import { Global } from '../../helpers/Global';
-import useAuth from '../../hooks/useAuth';
+import { UserList } from './UserList';
 
 export const People = () => {
-  const { auth } = useAuth();
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
   const [more, setMore] = useState(true);
@@ -45,46 +43,6 @@ export const People = () => {
     }
   }
 
-  const nextPage = () => {
-    let next = page + 1;
-    setPage(next);
-    getUsers(next);
-  }
-
-  const follow = async (userId) => {
-    //request ajax
-    const request = await fetch(Global.url + 'follow/savefollow', {
-      method: 'POST',
-      body: JSON.stringify({ followed: userId }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('token')
-      }
-    });
-
-    const data = await request.json();
-
-    if (data.status == 'success') {
-      setFollowing([...following, userId]);
-    }
-    //update state follow
-  }
-  const unfollow = async (userId) => {
-
-    const request = await fetch(Global.url + 'follow/unfollow/' + userId, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('token')
-      }
-    });
-    const data = await request.json();
-    if (data.status == 'success') {
-      let filter = following.filter(followingUserId => userId !== followingUserId);
-      setFollowing(filter);
-    }
-  }
-
 
   return (
     <>
@@ -93,67 +51,18 @@ export const People = () => {
         <button className="content__button">Mostrar nuevas</button>
       </header>
 
-      <div className="content__posts">
+      <UserList
+        users={users}
+        getUsers={getUsers}
+        following={following}
+        setFollowing={setFollowing}
+        more = {more}
+        loading = {loading}
+        page = {page}
+        setPage = {setPage}
+      />
 
-
-        {users.map(user => {
-          return (
-            <article className="posts__post" key={user._id}>
-
-              <div className="post__container">
-
-                <div className="post__image-user">
-                  <a href="#" className="post__image-link">
-                    {user.image != "default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Pic Profile" />}
-                    {user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Pic Profile" />}
-                  </a>
-                </div>
-
-                <div className="post__body">
-
-                  <div className="post__user-info">
-                    <a href="#" className="user-info__name">{user.name} {user.surname}</a>
-                    <span className="user-info__divider"> | </span>
-                    <a href="#" className="user-info__create-date">{user.created_at}</a>
-                  </div>
-
-                  <h4 className="post__content">{user.bio}</h4>
-
-                </div>
-
-              </div>
-
-              {user._id != auth._id &&
-
-                <div className="post__buttons">
-                  {!following.includes(user._id) &&
-                    <button className="post__button post__button--green"
-                      onClick={() => follow(user._id)}>
-                      Follow
-                    </button>
-                  }
-                  {following.includes(user._id) &&
-                    <button className="post__button"
-                      onClick={() => unfollow(user._id)}>
-                      Unfollow
-                    </button>
-                  }
-
-                </div>
-              }
-            </article>
-          );
-        })}
-
-      </div>
-      {loading ? <div>Loading...</div> : ''}
-      {more &&
-        <div className="content__container-btn">
-          <button className="content__btn-more-post" onClick={nextPage}>
-            Show more people
-          </button>
-        </div>
-      }
+     
       <br />
     </>
   )
---
+++ frontend/src/components/user/UserList.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Global } from '../../helpers/Global';
+import useAuth from '../../hooks/useAuth';
+import avatar from '../../assets/img/user.png';
+export const UserList = ({ users, getUsers, following, setFollowing, more, loading, page, setPage }) => {
+  const { auth } = useAuth();
+
+  const nextPage = () => {
+    let next = page + 1;
+    setPage(next);
+    getUsers(next);
+  }
+  const follow = async (userId) => {
+    //request ajax
+    const request = await fetch(Global.url + 'follow/savefollow', {
+      method: 'POST',
+      body: JSON.stringify({ followed: userId }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      }
+    });
+
+    const data = await request.json();
+
+    if (data.status == 'success') {
+      setFollowing([...following, userId]);
+    }
+    //update state follow
+  }
+  const unfollow = async (userId) => {
+
+    const request = await fetch(Global.url + 'follow/unfollow/' + userId, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      }
+    });
+    const data = await request.json();
+    if (data.status == 'success') {
+      let filter = following.filter(followingUserId => userId !== followingUserId);
+      setFollowing(filter);
+    }
+  }
+
+  return (
+    <>
+      <div className="content__posts">
+
+
+        {users.map(user => {
+          return (
+            <article className="posts__post" key={user._id}>
+
+              <div className="post__container">
+
+                <div className="post__image-user">
+                  <a href="#" className="post__image-link">
+                    {user.image != "default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Pic Profile" />}
+                    {user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Pic Profile" />}
+                  </a>
+                </div>
+
+                <div className="post__body">
+
+                  <div className="post__user-info">
+                    <a href="#" className="user-info__name">{user.name} {user.surname}</a>
+                    <span className="user-info__divider"> | </span>
+                    <a href="#" className="user-info__create-date">{user.created_at}</a>
+                  </div>
+
+                  <h4 className="post__content">{user.bio}</h4>
+
+                </div>
+
+              </div>
+
+              {user._id != auth._id &&
+
+                <div className="post__buttons">
+                  {!following.includes(user._id) &&
+                    <button className="post__button post__button--green"
+                      onClick={() => follow(user._id)}>
+                      Follow
+                    </button>
+                  }
+                  {following.includes(user._id) &&
+                    <button className="post__button"
+                      onClick={() => unfollow(user._id)}>
+                      Unfollow
+                    </button>
+                  }
+
+                </div>
+              }
+            </article>
+          );
+        })}
+
+      </div>
+      {loading ? <div>Loading...</div> : ''}
+      {more &&
+        <div className="content__container-btn">
+          <button className="content__btn-more-post" onClick={nextPage}>
+            Show more people
+          </button>
+        </div>
+      }
+    </>
+  )
+}
